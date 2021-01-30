3e1f7d97a33713834ca93a9234cacbe7a06f78d2
lucianne302 <lucianne21@comcast.net>
2021-01-30 18:18:29 -0500

    correction

--- client/src/pages/Newsletter.js
+++ client/src/pages/Newsletter.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 //import { useQuery } from '@apollo/react-hooks';
 
-function About() {
+function Newsletter() {
   return (
   <>
     <div className="container my-1">
@@ -11,19 +11,16 @@ function About() {
             ← Back to Products
         </Link>
           <>
-            <h2> About </h2>
+            <h2> Newsletter </h2>
                 <div className="my-2">
                     <div className="flex-row">
-                        <h3> About the Company </h3>
+                        <h3> GCF Clothing is Give Christ First </h3>
                     </div>
                     <div className="flex-row">
-                        <p> At Give Christ First clothing, we provide faith-fueled clothing to faith-filled individuals because we believe your clothes should match your message. We spread the good news of Christ in every piece in our collection for men, women, and children alike. Founded in 2019, GCF Clothing has made a mission to make Christian clothing more exciting and stylish.</p>
+                        <h2> Welcome! </h2>
                     </div>
                     <div className="flex-row">
-                        <h3> About the Owner </h3>
-                    </div>
-                    <div className="flex-row">
-                        <p> Hello! My name is Curtis Henderson and I created GCF Clothing as a way to express my faith through fashion. I've always been passionate about creating and designing new things. I've enjoyed creating flyers, websites, films, tv episodes, and so much more. I believe that we were all created to create and no matter how big or how small our creations may be, they are valuable and should be shared with the world!</p>
+                        <p> As of January 7, 2021, GCF Clothing owned and designed by Curtis Henderson has a new web page. Feel free to return every month to see our new designs and newsletter.</p>
                     </div>
                 </div>    
             </>
@@ -31,4 +28,4 @@ function About() {
     </>)
 };
 
-export default About;
+export default Newsletter;
