40264c7ad2ec009d53428defb9eb7ed821b6d9a7
chender93 <chender93@gmail.com>
2021-02-01 15:41:21 -0500

    Testing google analytics embedding

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
+import { renderButton, checkSignedIn } from "../utils/ganalytics"
 
 const history = createBrowserHistory();
 const trackingId = process.env.TRACKING_ID;
@@ -18,20 +19,49 @@ history.listen(location => {
 //import { useQuery } from '@apollo/react-hooks';
 
 function AdminPartial() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const updateSignin = (signedIn) => { //(3)
+    setIsSignedIn(signedIn);
+    if (!signedIn) {
+      renderButton();
+    }
+  };
+
+  const init = () => { //(2)
+    checkSignedIn()
+      .then((signedIn) => {
+        updateSignin(signedIn);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    window.gapi.load("auth2", init); //(1)
+  });
+
   return (
-  <>
-    <div className="container my-1">
+    <>
+      <div className="container my-1">
         <Link to="/">
-            ← Back to Home
+          ← Back to Home
         </Link>
-          <>
-            <h2> Welcome to the GCF Administrative Page! </h2>
-                <div className="my-2">
-                    <div className="flex-row">
-                        <p> This page will provide your with your web page (business) analytics.  What would you like to learn today? </p>
-                    </div>
-                </div>    
-            </>
+        <>
+          <h2> Welcome to the GCF Administrative Page! </h2>
+          <div className="my-2">
+            <div className="flex-row">
+              <p> This page will provide your with your web page (business) analytics.  What would you like to learn today? </p>
+              {!isSignedIn ? (
+                <div className="g-signin2" data-onsuccess="onSignIn"></div>
+                //<div id="signin-button"></div>
+              ) : (
+                  <div>Coming soon...</div>
+                )}
+            </div>
+          </div>
+        </>
       </div>
     </>)
 };
---
+++ client/src/utils/ganalytics.js
@@ -0,0 +1,39 @@
+const initAuth = () => {
+    return window.gapi.auth2.init({
+      client_id: "499571463242-fe3kom4nijdeun61k0dcjth5ebt8gbse.apps.googleusercontent.com",
+      scope: "https://www.googleapis.com/auth/analytics.readonly",
+    });
+  };
+
+  export const checkSignedIn = () => {
+    return new Promise((resolve, reject) => {
+      initAuth() //calls the previous function
+        .then(() => {
+          const auth = window.gapi.auth2.getAuthInstance(); //returns the GoogleAuth object
+          resolve(auth.isSignedIn.get()); //returns whether the current user is currently signed in
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
+  export const renderButton = () => {
+    window.gapi.signin2.render("signin-button", {
+      scope: "profile email",
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: "dark",
+      onsuccess: onSuccess,
+      onfailure: onFailure,
+    });
+  };
+  
+  const onSuccess = (googleUser) => {
+    console.log("Logged in as: " + googleUser.getBasicProfile().getName());
+  };
+  
+  const onFailure = (error) => {
+    console.error(error);
+  };
