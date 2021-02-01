2031d1b90b6590a558eae95e6d032fd432e85955
lucianne302 <lucianne21@comcast.net>
2021-02-01 14:22:11 -0500

    added made in USA on about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -17,7 +17,7 @@ function About() {
                         <h3> About the Company </h3>
                     </div>
                     <div className="flex-row">
-                        <p> At Give Christ First clothing, we provide faith-fueled clothing to faith-filled individuals because we believe your clothes should match your message. We spread the good news of Christ in every piece in our collection for men, women, and children alike. Founded in 2019, GCF Clothing has made a mission to make Christian clothing more exciting and stylish.</p>
+                        <p> At Give Christ First clothing, we provide faith-fueled clothing made in the USA.  Our clothing is mde for faith-filled individuals because we believe your clothes should match your message. We spread the good news of Christ in every piece in our collection for men, women, and children alike. Founded in 2019, GCF Clothing has made a mission to make Christian clothing more exciting and stylish.</p>
                     </div>
                     <div className="flex-row">
                         <h3> About the Owner </h3>
